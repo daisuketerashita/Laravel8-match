5a305b033c3a8eb54728029a816f94d07f9db6a5
寺下大典 <ekusiad.tera@icloud.com>
2022-05-18 21:29:59 +0900

    プロフィール画像の表示

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function show($id)
     {
-        return view('users.show');
+        $user = User::findorFail($id);
+
+        return view('users.show',['user' => $user]);
     }
 }
